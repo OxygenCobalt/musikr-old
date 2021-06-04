52a728dd9160381ecdb1bf2039cc8f87f3e9f68d
OxygenCobalt <oxycblt@abstractr.net>
2021-06-04 15:07:01 -0600

    id3: improve frame matching process
    Turn the frame matching process from a long series of IFs to a more
    optimal match statement. This should make this cumbersome ID
    comparison process slightly more efficent.

--- libmusikr/src/id3/frame/mod.rs
+++ libmusikr/src/id3/frame/mod.rs
@@ -39,9 +39,6 @@ pub(super) fn new(header: &TagHeader, data: &[u8]) -> Option<Box<dyn Id3Frame>> 
 
     let data = &data[10..(frame_header.frame_size + 10)];
 
-    // Now we have to manually go through and determine what kind of frame to create based
-    // on the frame id. There are many frame possibilities, so there are many if blocks.
-
     // TODO: Handle compressed frames
     // TODO: Handle duplicate frames
     // TODO: Handle unsynchonization
@@ -49,114 +46,78 @@ pub(super) fn new(header: &TagHeader, data: &[u8]) -> Option<Box<dyn Id3Frame>> 
     // TODO: Make frame creation return defaults when there isn't enough data
     // TODO: Add readable frame names
 
-    let frame_id = &frame_header.frame_id;
+    // Now we have to manually go through and determine what kind of frame to create based
+    // on the frame id. There are many frame possibilities, so there are many match arms.
 
-    // Unique File Identifier [Frames 4.1]
+    let frame: Box<dyn Id3Frame> = match frame_header.frame_id.as_str() {
+        // --- Text Information [Frames 4.2] ---
 
-    if frame_id == "UFID" {
-        return Some(Box::new(FileIdFrame::new(frame_header, data)));
-    }
+        // User-Defined Text Informations [Frames 4.2.6]
+        "TXXX" => Box::new(UserTextFrame::new(frame_header, data)),
 
-    // --- Text Information [Frames 4.2] ---
+        // Involved People List & Musician Credits List [Frames 4.2.2]
+        // These can all be mapped to the same frame [Including the legacy IPLS frame]
+        "IPLS" | "TIPL" | "TMCL" => Box::new(CreditsFrame::new(frame_header, data)),
 
-    // Involved People List & Musician Credits List [Frames 4.2.2]
-    // Both of these lists can correspond to the same frame.
+        // Apple's WFED (Podcast URL), MVNM (Movement Name), MVIN (Movement Number),
+        // and GRP1 (Grouping) frames are all actually text frames
+        "WFED" | "MVNM" | "MVIN" | "GRP1" => Box::new(TextFrame::new(frame_header, data)),
+        
+        // Generic Text Information
+        id if id.starts_with('T') => Box::new(TextFrame::new(frame_header, data)),
 
-    if frame_id == "IPLS" || frame_id == "TIPL" || frame_id == "TMCL" {
-        return Some(Box::new(CreditsFrame::new(frame_header, data)));
-    }
+        // --- URL Link [Frames 4.3] ---
 
-    // All text frames begin with 'T', but apple's proprietary WFED (Podcast URL), MVNM (Movement Name),
-    // MVIN (Movement Number), and GRP1 (Grouping) frames are all text frames as well.
-    if frame_id.starts_with('T')
-        || frame_id == "WFED"
-        || frame_id == "MVNM"
-        || frame_id == "MVIN"
-        || frame_id == "GRP1"
-    {
-        // User-Defined Text Info [Frames 4.2.6]
+        // User-Defined URL Link [Frames 4.3.2]
+        "WXXX" => Box::new(UserUrlFrame::new(frame_header, data)),
 
-        if frame_id == "TXXX" {
-            return Some(Box::new(UserTextFrame::new(frame_header, data)));
-        }
+        // Generic URL Link
+        id if id.starts_with('W') => Box::new(UrlFrame::new(frame_header, data)),
 
-        return Some(Box::new(TextFrame::new(frame_header, data)));
-    }
+        // --- Other Frames ---
 
-    // --- URL Link [Frames 4.3] ---
+        // Unique File Identifier [Frames 4.1]
+        "UFID" => Box::new(FileIdFrame::new(frame_header, data)),
 
-    if frame_id.starts_with('W') {
-        // User-Defined URL [Frames 4.3.2]
+        // Event timing codes [Frames 4.5]
+        "ETCO" => Box::new(EventTimingCodesFrame::new(frame_header, data)),
 
-        if frame_id == "WXXX" {
-            return Some(Box::new(UserUrlFrame::new(frame_header, data)));
-        }
+        // Unsynchronized Lyrics [Frames 4.8]
+        "USLT" => Box::new(UnsyncLyricsFrame::new(frame_header, data)),
 
-        return Some(Box::new(UrlFrame::new(frame_header, data)));
-    }
+        // Unsynchronized Lyrics [Frames 4.9]
+        "SYLT" => Box::new(SyncedLyricsFrame::new(frame_header, data)),
 
-    // Event timing codes [Frames 4.5]
+        // Comments [Frames 4.10]
+        "COMM" => Box::new(CommentsFrame::new(frame_header, data)),
 
-    if frame_id == "ETCO" {
-        return Some(Box::new(EventTimingCodesFrame::new(frame_header, data)));
-    }
+        // TODO: Relative Volume Adjustment [Frames 4.11]
 
-    // Unsynchronized Lyrics [Frames 4.8]
+        // Attatched Picture [Frames 4.14]
+        "APIC" => Box::new(AttatchedPictureFrame::new(frame_header, data)),
 
-    if frame_id == "USLT" {
-        return Some(Box::new(UnsyncLyricsFrame::new(frame_header, data)));
-    }
+        // General Encapsulated Object [Frames 4.15]
+        "GEOB" => Box::new(GeneralObjectFrame::new(frame_header, data)),
 
-    // Synchronized Lyrics [Frames 4.9]
+        // Play Counter [Frames 4.16]
+        "PCNT" => Box::new(PlayCounterFrame::new(frame_header, data)),
 
-    if frame_id == "SYLT" {
-        return Some(Box::new(SyncedLyricsFrame::new(frame_header, data)));
-    }
+        // Popularimeter [Frames 4.17]
+        "POPM" => Box::new(PopularimeterFrame::new(frame_header, data)),
 
-    // Comments [Frames 4.10]
+        // TODO: [Maybe] Linked info frame [Frames 4.20]
+        // TODO: Terms of use frame [Frames 4.22]
+        // TODO: Ownership frame [Frames 4.23]
+        // TODO: [Maybe] Commercial Frame [Frames 4.24]
 
-    if frame_id == "COMM" {
-        return Some(Box::new(CommentsFrame::new(frame_header, data)));
-    }
+        // Private Frame [Frames 4.27]
+        "PRIV" => Box::new(PrivateFrame::new(frame_header, data)),
 
-    // TODO: Relative Volume Adjustment [Frames 4.11]
+        // Unknown, return raw frame
+        _ => Box::new(RawFrame::from(frame_header, data)),
+    };
 
-    // Attatched Picture [Frames 4.14]
-
-    if frame_id == "APIC" {
-        return Some(Box::new(AttatchedPictureFrame::new(frame_header, data)));
-    }
-
-    // General Encapsulated Object [Frames 4.15]
-
-    if frame_id == "GEOB" {
-        return Some(Box::new(GeneralObjectFrame::new(frame_header, data)));
-    }
-
-    // Play Counter [Frames 4.16]
-
-    if frame_id == "PCNT" {
-        return Some(Box::new(PlayCounterFrame::new(frame_header, data)));
-    }
-
-    // Popularimeter  [Frames 4.17]
-
-    if frame_id == "POPM" {
-        return Some(Box::new(PopularimeterFrame::new(frame_header, data)));
-    }
-
-    // TODO: [Maybe] Linked info frame [Frames 4.20]
-    // TODO: Terms of use frame [Frames 4.22]
-    // TODO: Ownership frame [Frames 4.23]
-    // TODO: [Maybe] Commercial Frame [Frames 4.24]
-    // Private Frame [Frames 4.27]
-
-    if frame_id == "PRIV" {
-        return Some(Box::new(PrivateFrame::new(frame_header, data)));
-    }
-
-    // Not supported, return a raw frame
-    Some(Box::new(RawFrame::from(frame_header, data)))
+    Some(frame)
 }
 
 pub struct Id3FrameHeader {
