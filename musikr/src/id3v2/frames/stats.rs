ca1927b64a21c3c720eed8107eab2ba8c90a4690
OxygenCobalt <oxycblt@abstractr.net>
2021-06-20 14:05:24 -0600

    id3v2: add stats frame rendering
    Add rendering to the PCNT and POPM frames.

--- musikr/src/id3v2/frames/stats.rs
+++ musikr/src/id3v2/frames/stats.rs
@@ -1,4 +1,5 @@
 use crate::err::{ParseError, ParseResult};
+use crate::id3v2::TagHeader;
 use crate::id3v2::frames::{Frame, FrameFlags, FrameHeader};
 use crate::raw;
 use crate::string::{self, Encoding};
@@ -31,6 +32,7 @@ impl PopularimeterFrame {
 
     pub(crate) fn parse(header: FrameHeader, data: &[u8]) -> ParseResult<Self> {
         if data.len() < 2 {
+            // Must be at least an empty owner string and a rating
             return Err(ParseError::NotEnoughData);
         }
 
@@ -64,6 +66,18 @@ impl PopularimeterFrame {
     pub fn plays(&self) -> u64 {
         self.plays
     }
+    
+    pub fn email_mut(&mut self) -> &mut String {
+        &mut self.email
+    }
+
+    pub fn rating_mut(&mut self) -> &mut u8 {
+        &mut self.rating
+    }
+
+    pub fn plays_mut(&mut self) -> &mut u64 {
+        &mut self.plays
+    }
 
     pub fn rating_simple(&self) -> u8 {
         match self.rating {
@@ -92,6 +106,24 @@ impl Frame for PopularimeterFrame {
 
     fn key(&self) -> String {
         format!["{}:{}", self.id(), self.email]
+    }
+
+    fn is_empty(&self) -> bool {
+        false // POPM Can never be empty
+    }
+
+    fn render(&self, _: &TagHeader) -> Vec<u8> {
+        let mut result = Vec::new();
+
+        result.extend(string::render_terminated(Encoding::Latin1, &self.email));
+        result.push(self.rating);
+
+        // Save some space by omitting the play count if zero
+        if self.plays > 0 {
+            result.extend(render_play_count(self.plays));
+        }
+
+        result
     }
 }
 
@@ -144,6 +176,10 @@ impl PlayCounterFrame {
     pub fn plays(&self) -> u64 {
         self.plays
     }
+
+    pub fn plays_mut(&mut self) -> &mut u64 {
+        &mut self.plays
+    }
 }
 
 impl Frame for PlayCounterFrame {
@@ -162,6 +198,14 @@ impl Frame for PlayCounterFrame {
     fn key(&self) -> String {
         self.id().clone()
     }
+
+    fn is_empty(&self) -> bool {
+        false // Can never be empty.
+    }
+
+    fn render(&self, _: &TagHeader) -> Vec<u8> {
+        render_play_count(self.plays)
+    }
 }
 
 impl Display for PlayCounterFrame {
@@ -174,6 +218,21 @@ impl Default for PlayCounterFrame {
     fn default() -> Self {
         Self::with_flags(FrameFlags::default())
     }
+}
+
+fn render_play_count(play_count: u64) -> Vec<u8> {
+    let bytes = play_count.to_be_bytes();
+    
+    for i in 0..4 {
+        // The size is larger than a 4-bytes, so return the first four bytes
+        // plus the populated byte we just found.
+        if bytes[i] > 0 {
+            return bytes[i..].into();
+        }
+    }
+
+    // Otherwise return the first four bytes, the hard-limit by the spec.
+    bytes[4..].into()
 }
 
 #[cfg(test)]
@@ -200,5 +259,32 @@ mod tests {
         let frame = PlayCounterFrame::parse(FrameHeader::new("PCNT"), PCNT_DATA).unwrap();
 
         assert_eq!(frame.plays(), 0x1616)
+    }
+
+    #[test]
+    fn render_popm() {
+        let mut frame = PopularimeterFrame::new();
+        frame.email_mut().push_str("test@test.com");
+        *frame.rating_mut() = 0x80;
+        *frame.plays_mut() = 0x1616;
+
+        assert!(!frame.is_empty());
+        assert_eq!(frame.render(&TagHeader::with_version(4)), POPM_DATA);
+    }
+
+    #[test]
+    fn render_pcnt() {
+        let mut frame = PlayCounterFrame::new();
+        *frame.plays_mut() = 0x1616;
+        
+        assert!(!frame.is_empty());
+        assert_eq!(frame.render(&TagHeader::with_version(4)), PCNT_DATA);
+    }
+
+    #[test]
+    fn render_large_play_counts() {
+        let plays: u64 = 0x123456789ABCD;
+
+        assert_eq!(render_play_count(plays), b"\x01\x23\x45\x67\x89\xAB\xCD");
     }
 }
